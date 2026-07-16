7fe9abc5f3fb164dedf31da68efbe3760f697073
SeungWoo1222 <wlstmddn98@naver.com>
2026-07-16 14:30:08 +0900

    chore: .gitignore 정리 (build 산출물·env 제외)

--- build/resources/main/db/migration/V1__create_regions.sql
+++
@@ -1,9 +0,0 @@
--- 법정동 코드 기반 지역 테이블
-CREATE TABLE regions (
-    id            BIGSERIAL PRIMARY KEY,
-    sido_name     VARCHAR(20)  NOT NULL,  -- 시도 (서울특별시)
-    sigungu_name  VARCHAR(20)  NOT NULL,  -- 시군구 (강남구)
-    sigungu_code  VARCHAR(5)   NOT NULL UNIQUE  -- 국토부 시군구 코드
-);
-
-CREATE INDEX idx_regions_sido ON regions (sido_name);
--- build/resources/main/db/migration/V7__add_trigram_index_apartment_complexes.sql
+++
@@ -1,5 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-
-CREATE INDEX idx_complexes_name_trgm
-    ON apartment_complexes
-    USING GIN (complex_name gin_trgm_ops);
